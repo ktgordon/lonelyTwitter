12c6f6a2443b89ee08dea43ee30f897ded2e51d5
ktgordon <ktgordon@ualberta.ca>
2018-01-17 19:24:12 -0700

    lab2 work. done?

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Happy.java
@@ -0,0 +1,10 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import Mood;
+
+/**
+ * Created by kevingordon on 2018-01-17.
+ */
+
+public class Happy extends Mood {
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Mood.java
@@ -0,0 +1,8 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by kevingordon on 2018-01-17.
+ */
+
+public class Mood {
+}
